06e3db50ae1d0e3f14c5e2c119751fb2b0e10588
Sebastian Markbage <sema@fb.com>
2014-11-19 22:53:44 -0800

    Switch refs proposal from first class to callbacks
    Optional future progress could involve Observable middle men like Rx
    subjects.

--- 01 - Core/06 - Refs.js
+++ 01 - Core/06 - Refs.js
@@ -1,85 +1,99 @@
+// This is a refs proposal that uses callbacks instead of strings. The callback
+// can be easily statically typed and solves most use cases.
+
+// When a ref is attached, it gets passed the instance as the first argument.
+// When a ref is detached, the callback is invoked with null as the argument.
+
 // refs on DOM nodes gives the DOM node handle directly, not an intermediate
 // form.
 
 class Foo {
 
-  // A ref is declared
-  myDivRef = React.createRef();
+  myDivRef : ?HTMLDivElement;
 
-  // Promise API for refs
   handleTick() {
-    this.myDivRef.then(myDivNode => {
-      this.setState({ width: myDivNode.offsetWidth });
-    });
-  }
-
-  // Alternative syntax using future ES7 async/await syntax
-  async handleTick() {
-    this.setState({ width: (await this.myDivRef).offsetWidth });
+    this.setState({ width: this.myDivRef.offsetWidth });
   }
 
   render() {
     return (
       <C tick={this.handleTick}>
-        <div ref={this.myDivRef} />
-        <CustomComponent context={this.myDivRef} />
+        <div ref={node => this.myDivRef = node} />
+        <CustomComponent context={() => this.myDivRef} />
       </C>
     );
   }
 
 }
+
+// It's possible to distinguish whether a ref as ever been mounted or if it has
+// been mounted during this particular reconciliation phase.
+
+class Component {
+ 
+  buttonWasEverMounted : boolean;
+  buttonWasMountedThisPass : boolean;
+  button : ?Button;
+ 
+  mountButtonRef = button => {
+    if (button) {
+      this.buttonWasEverMounted = true;
+      this.buttonWasMountedThisPass = true;
+    }
+    this.button = button;
+  }
+ 
+  componentWillMount() {
+    this.componentWillUpdate();
+  }
+
+  componentDidMount() {
+    this.componentDidUpdate();
+  }
+ 
+  componentWillUpdate() {
+    this.buttonWasMountedThisPass = false;
+  }
+
+  componentDidUpdate() {
+    if (this.buttonWasEverMounted) {
+      console.log('button was mounted at least once');
+    }
+    if (this.buttonWasMountedThisPass) {
+      console.log('button was mounted during this render pass');
+    }
+  }
+ 
+  render() {
+    return <Button ref={this.mountButtonRef} />;
+  }
+  
+}
+
+// In a future world where every callback is also implemented as an Observer,
+// we can pass a subject to the ref to build Observable compositions on top
+// of a ref.
 
 class Foo {
-  refs = new React.RefMap();
 
-  handleTick() {
-    this.refs.get('myDiv').then(myDivNode => {
-      this.setState({ width: myDivNode.offsetWidth });
-    });
+  myTick = new Rx.Subject();
+  myDiv = new Rx.Subject();
+
+  observe() {
+    var widths = this.myDiv.map(myDivRef => myDivRef.offsetWidth);
+    return {
+      width: this.myTick.combineLatest(widths, (e, width) => width)
+    };
   }
 
   render() {
     return (
-      <C tick={this.handleTick}>
-        <div ref={this.refs.get('myDiv')} />
-        <CustomComponent context={this.refs.get('myDiv')} />
+      <C tick={this.myTick}>
+        <div ref={this.myDiv} />
+        <CustomComponent context={this.myDiv} />
       </C>
     );
   }
+
 }
 
-// We also probably need to provide a synchronous API as an upgrade path:
-
-  handleTick() {
-    var myDivNode = this.myDivRef.doExpensiveWorkRightNowAndLetMeHaveMyNodeNow();
-    this.setState({ width: myDivNode.offsetWidth });
-  }
-
-  handleTick() {
-    this.refs.get('myDiv').then(myDivNode => {
-      this.setState({ width: myDivNode.offsetWidth });
-    }, error => {
-      this.setState({ width: 0 });
-    });
-  }
-
-
-// An alternative idea... Make elements into ref-promises.
-
-  render() {
-    var foo = <Foo />;
-    return <div onClick={() => foo.then(inst => inst.doX())}>{foo}</div>;
-  }
-
-// This also provides a nice reset functionality if the ref is ever swapped out.
-
-  handleClick() {
-    this.foo.then(inst => inst.doX());
-  }
-
-  render() {
-    this.foo = <Foo />; // ugh side-effect in render (puke)
-    return <div onClick={this.handleClick}>{this.foo}</div>;
-  }
-
-// This also works nice with multiple owners.

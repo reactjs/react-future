429e04340f3469556cc99666e59dd8a20dac6d57
Sebastian Markbage <sema@fb.com>
2015-02-12 20:44:09 -0800

    Added example of a declarative component model
    We're investigating the possibility of state simply being a reduction of
    all input signals instead of - or complementing - an imperative state
    machine.
    
    This doesn't require any low-level special data types like Promises,
    Observables, Streams nor Channels.
    
    Since we're doing it at a Component level granularity, it should be more
    performant than aforementioned solutions which require fine-grained value
    propagation and transformers.

---
+++ 09 - Reduce State/01 - Declarative Component Module.js
@@ -0,0 +1,52 @@
+// Declarative React Component
+
+export default let FancyButton = {
+
+  // getInitialState
+  initialize(props) {
+    return {
+      count: Math.round(props.width / 10),
+      name: null
+    };
+  },
+
+  // downwards render output
+  render(props, state) {
+    return (
+      <div>
+        {state.name || 'Loading name...'}
+        Clicked {state ? state.count : 0} times
+        <button signal="button" style={{ width: props.width }} />
+      </div>
+    );
+  },
+
+  // subscribe to third party data asynchronously
+  observe(props, state) {
+    return {
+      data: request(props.uri)
+    };
+  },
+
+  // reduce whenever a signal fires into new state
+  reduce(props, state, signals) {
+    let data = singals.data || state.data;
+    let click = signals.button.click;
+    if (click) {
+      if (click.button === 1) {
+        return { count: 0, name: data };
+      }
+      return { count: previousState.count + 1, name: data };
+    }
+    return state;
+  },
+
+  // upwards signals back to the owner (events)
+  output(props, state, signals) {
+    return {
+      name: state.name,
+      click: signals.button.click
+    };
+  }
+
+};
